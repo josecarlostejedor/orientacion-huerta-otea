42b89ed75bd88e2f491d9ebc132d497534a4ca2c
josecarlostejedor <josecarlostejedor@gmail.com>
2026-03-05 11:43:28 +0100

    Add files via upload

---
+++ src/lib/utils.ts
@@ -0,0 +1,14 @@
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function normalizeString(str: string): string {
+  return str
+    .toLowerCase()
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '')
+    .trim();
+}
